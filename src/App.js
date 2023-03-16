2508e4bc328b83ce9f5d54dfd838a345adc329a2
Muddasir Pathan <113206188+mudassirpathan1@users.noreply.github.com>
2023-03-16 12:58:17 +0530

    test copmpleted

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import First from './components/First';
+import Second from './components/Second';
+
+function App() {
+  return (
+    <div className="App">
+      <First />
+     <h1>Welcome to Shop</h1>
+     <Second />
+     
+    </div>
+    
+  );
+}
+
+export default App;
